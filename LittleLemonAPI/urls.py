097f9b6839fe9dbfc857a2c1f1c2271c84870796
Abror <boqiyerabror@gmail.com>
2024-07-18 13:47:21 +0500

    serializers

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('menu-items', MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', SingleItemMenuView.as_view()),
+    path('menu-items', menu_items),
+    path('menu-items/<int:pk>', single_item),    
 ]

0ddf56b28a964de09fb5fdd7184753ad818267ba
Abror <boqiyerabror@gmail.com>
2024-07-18 18:31:53 +0500

    fn based serialiser

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,18 +1,25 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
-from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def menu_items(request):
-    items = MenuItem.objects.all()
-    serialized_item = MenuItemSerializer(items, many=True)
-    return Response(serialized_item.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.all()
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
 
 @api_view()
-def single_item(request, pk):
-    item = get_object_or_404(MenuItem,id=pk)
+def single_item(request, id):
+    item = get_object_or_404(MenuItem,pk=id)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
